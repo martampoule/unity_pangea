c2f387eeef05149f2f828e84a2886e9841d2fc3c
marta <marta.cattaneo.96@gmail.com>
2019-01-04 16:11:02 +0100

    pangea fino ai giorni nostri

---
+++ Assets/Change.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Change : MonoBehaviour {
+
+	public Material[] material;
+	Renderer rend;
+	static int index = 0;
+	private int currentDataSet = 0;
+
+
+	string[] text = {
+		"Giurassico Inferiore",     // 0
+		"Giurassico Medio",     // 1
+		"Tardo Giurassico",     // 2
+		"Cretaceo Inferiore", 
+		"Cretaceo Medio",
+		"Cretaceo Superiore",
+		"Cretaceo Terziario",
+		"Eocene",
+		"Oligocene",
+		"Miocene",
+		"Presente",
+	};
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+		rend = GetComponent<Renderer> ();
+		rend.enabled = true;
+		rend.sharedMaterial = material [currentDataSet];
+
+		AggiornaLabel(currentDataSet);
+	}
+
+
+	void AggiornaLabel(int t){
+		TextMesh textObject = GameObject.Find("MyText").GetComponent<TextMesh>();
+		textObject.text = text[t];
+	}
+
+
+	// Update is called once per frame
+
+
+	void OnGUI(){
+		Event e = Event.current;
+		if (e.type == EventType.MouseDown){			
+			currentDataSet = (currentDataSet + 1) % 11;
+			rend.sharedMaterial = material[currentDataSet];
+
+			// carica i dati per i tasti:
+
+
+
+			AggiornaLabel(currentDataSet);
+
+		}			
+	}
+				
+	}
+
+
+
---
+++ Assets/sphere.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+	Material m_Material;
+
+	void Start()
+	{
+		//Fetch the Material from the Renderer of the GameObject
+		m_Material = GetComponent<Renderer>().material;
+		print("Materials " + Resources.FindObjectsOfTypeAll(typeof(Material)).Length);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			//Output the amount of materials before GameObject is destroyed
+			print("Materials " + Resources.FindObjectsOfTypeAll(typeof(Material)).Length);
+			//Destroy GameObject
+			Destroy(gameObject);
+		}
+	}
+
+	void OnMouseOver()
+	{
+		// Change the Color of the GameObject when the mouse hovers over it
+		m_Material.color = Color.red;
+	}
+
+	void OnMouseExit()
+	{
+		//Change the Color back to white when the mouse exits the GameObject
+		m_Material.color = Color.white;
+	}
+
+	void OnDestroy()
+	{
+		//Destroy the instance
+		Destroy(m_Material);
+		//Output the amount of materials to show if the instance was deleted
+		print("Materials " + Resources.FindObjectsOfTypeAll(typeof(Material)).Length);
+	}
+}
